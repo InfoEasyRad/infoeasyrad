d290f0c39da48ffa8b249788087341987294c921
InfoEasyRad <cestrada8712@gmail.com>
2026-08-09 22:59:39 -0600

    Límites: Básico 60 y Pro 300 informes/mes (api/status.js)

--- api/status.js
+++ api/status.js
@@ -6,7 +6,7 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_KEY
 );
 
-const LIMITES = { trial: null, gratis: 5, basico: 50, pro: 200, clinica: null, starter: 50, ilimitado: null };
+const LIMITES = { trial: null, gratis: 5, basico: 60, pro: 300, clinica: null, starter: 60, ilimitado: null };
 
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
